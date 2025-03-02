e73a8f45aa28b1ff368eaa4381487eb65228ed50
Shining17 <bbvv777@naver.com>
2025-03-03 02:55:29 +0900

    localhost test

--- msa-news/src/main/java/com/example/msasbnews/config/WebSocketConfig.java
+++ msa-news/src/main/java/com/example/msasbnews/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-               .setAllowedOrigins("*")  // 프론트엔드 주소
+               .setAllowedOrigins("http://localhost:3000")  // 프론트엔드 주소
                .withSockJS();
     }
 }
