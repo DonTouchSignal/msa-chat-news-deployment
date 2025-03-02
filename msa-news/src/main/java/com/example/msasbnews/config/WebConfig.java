e75d2fa930410ad3db589499fe9f85410f651eb4
Shining17 <bbvv777@naver.com>
2025-03-03 02:07:03 +0900

    프론트 ip 수정

--- msa-news/src/main/java/com/example/msasbnews/config/WebConfig.java
+++ msa-news/src/main/java/com/example/msasbnews/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://34.222.150.136")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
